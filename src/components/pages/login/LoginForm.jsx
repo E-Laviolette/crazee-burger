0367b995853440723cae20c12e1cd6d3f36f61c7
E-Laviolette <emmanuel.laviolette@live.fr>
2023-11-22 18:27:16 +0100

    ♻️ F03: LOGIN > refacto Button and Input

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
-import styled from 'styled-components';
-import { BsPersonCircle } from "react-icons/bs";
-import { IoChevronForward } from "react-icons/io5"
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { IoChevronForward } from "react-icons/io5"
+import { BsPersonCircle } from "react-icons/bs";
+import Input from '../../reusable-ui/Input';
+import Button from '../../reusable-ui/Button';
 
 export default function LoginForm() {
     // state
@@ -25,19 +27,17 @@ export default function LoginForm() {
             <h1>Bienvenue chez nous !</h1>
             <hr />
             <h2>Connectez-vous</h2>
-            <div className="input-with-icon">
-                <BsPersonCircle className="icon"/>
-                <input
-                value={inputValue}
-                type="text"
-                placeholder="Entrez votre prénom"
+            <Input 
+                value={inputValue} 
                 onChange={handleChange}
-                required />
-            </div>
-            <button className="button-with-icon">
-                <span>Accéder à mon espace</span>
-                <IoChevronForward className="icon" />
-            </button>
+                placeholder={"Entrez votre prénom"} 
+                required
+                Icon={<BsPersonCircle className="icon" />}
+            />
+
+            <Button
+                Icon={<IoChevronForward className="icon" />}    
+            />
         </LoginFormStyled>
   )
 }
@@ -68,80 +68,9 @@ const LoginFormStyled = styled.form`
         font-size: 36px;
     }
 
-    .input-with-icon {
-        background-color: white;
-        border-radius: 5px;
-        display: flex;
-        align-items: center;
-        padding: 18px 24px;
-        margin: 18px 0;
+    
 
-        .icon {
-            font-size: 15px;
-            margin-right: 8px;
-            color: #93a2b1;
-        }
-        
-        input {
-            border: none;
-            font-size: 15px;
-            color: #17161a;
-            width: 100%;
-        }
-
-        &::placeholder {
-            background: white;
-            color: lightgrey;
-        }
-
-
-    }
-
-    .button-with-icon {
-        width: 100%;
-        border: 1px solid red;
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        white-space: nowrap;
-        text-decoration: none;
-        line-height: 1;
-
-        padding: 18px 24px;
-        border-radius: 5px;
-        font-size: 15px;
-        font-weight: 800;
-        color: white;
-        background-color: #ff9f1b;
-        border: 1px solid #ff9f1b;
-
-        &:hover:not(:disabled) {
-            background-color: white;
-            color: #ff9f1b;
-            border: 1px solid #ff9f1b;
-            transition: all 200ms ease-out;
-        }
-
-        &:active {
-            color: white;
-            background-color: #ff9f1b;
-            border: 1px solid #ff9f1b;
-        }
-
-        &:disabled {
-            opacity: 0.6;
-            cursor: not-allowed;
-        }
-
-        .icon {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-size: 15px;
-            margin-left: 10px;
-        }
-    }
+    
 `;
 
 // export default LoginFormStyled;

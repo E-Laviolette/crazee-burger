5ab347dffdde8cb8dc41c7bcb2a2a76436cd8797
E-Laviolette <emmanuel.laviolette@live.fr>
2023-11-29 17:37:05 +0100

    🚧 F05: ORDER > MENU > Add grid layout

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu"
 
 export default function Menu() {
 
-    const [menu, setMenu] = useState(fakeMenu)
+    const [menu, setMenu] = useState(fakeMenu2)
 
     return (
         <MenuStyled className="menu">
-            
+            {menu.map((product) => {
+              return <div className="product">{product.title}</div>
+            })}
         </MenuStyled>
     )
 }
 
 const MenuStyled = styled.div`
-        background: purple;
+    background: purple;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-row-gap: 60px;
+    padding: 50px 50px 150px;
+    justify-items: center;
+
+    .product {
+      background: red;
+      width: 240px;
+      height: 330px;
+
+    }
 `;

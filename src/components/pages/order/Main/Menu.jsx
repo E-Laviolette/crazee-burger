c77e2b940310102bcfc2f8f8da1392b41f31181d
E-Laviolette <emmanuel.laviolette@live.fr>
2023-11-29 17:17:32 +0100

    🍱 F05: Add assets

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import styled from 'styled-components';
 
 export default function Menu() {
-  return (
-      <MenuStyled>
-          Menu
-      </MenuStyled>
-  )
+
+    const [menu, setMenu] = useState(fakeMenu)
+
+    return (
+        <MenuStyled className="menu">
+            
+        </MenuStyled>
+    )
 }
 
 const MenuStyled = styled.div`

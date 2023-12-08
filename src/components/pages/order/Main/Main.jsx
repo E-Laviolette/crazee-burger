2b20bf33df96dab2846ae2bd43f20109478ac10a
E-Laviolette <emmanuel.laviolette@live.fr>
2023-12-08 21:56:09 +0100

    💄 F07: MAIN > Add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,28 +6,49 @@ export default function Main() {
   return (
     <MainStyled className="main">
         {/* <div className="basket">Basket</div> */}
-        <Menu />
+        <div className="menu-and-admin">
+          <Menu />
+          <div className="admin">Admin</div>
+        </div>
     </MainStyled>
   )
 }
 
 
 const MainStyled = styled.div`
+    border: 1px solid green;
     background: ${theme.colors.background_white};
-    flex: 1;
+    height: calc()(95vh - 10vh);
 
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
     display: grid;
-    grid-template-columns: 1fr;
+    grid-template-columns: /*25%*/ 1fr;
 
     overflow-y: scroll;
 
     /* .basket {
         background: pink;
-      } */
+    } */
       
+    .menu-and-admin {
+        position: relative;
+        overflow-y: hidden;
+        display: grid;
+        border-bottom-left-radius: ${theme.borderRadius.extraRound};
+        border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+        .admin {
+            background: red;
+            height: 250px;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    }
+    
 
 `;

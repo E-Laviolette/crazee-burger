3045b593b0979518c3ccedb486bedbb38ff0eeb2
E-Laviolette <emmanuel.laviolette@live.fr>
2023-12-19 14:57:50 +0100

    💄 F07: ADMIN > Add AdminTabs and AdminPanel

---
+++ src/components/pages/order/Main/admin/AdminTabs.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { theme } from "../../../../../theme";
+
+export default function AdminTabs() {
+
+    return (
+      <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+    )
+}
+
+const AdminTabsStyled = styled.div`
+
+`;
